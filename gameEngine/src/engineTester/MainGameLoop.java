dacfe4dc3214d3cd91cc978200a7df8d563771d2
Viktor Parvanov <victor.parvanov@nevexis.com>
2019-12-29 21:12:45 +0200

    Added implementation to read 3d models

--- gameEngine/src/engineTester/MainGameLoop.java
+++ gameEngine/src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -20,22 +21,17 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 
-		float[] vertices = { -0.5f, 0.5f, 0, -0.5f, -0.5f, 0, 0.5f, -0.5f, 0, 0.5f, 0.5f, 0, };
 
-		int[] indices = { 0, 1, 3, 3, 1, 2 };
-
-		float[] textureCoords = { 0, 0, 0, 1, 1, 1, 1, 0 };
-
-		RawModel rawModel = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture modelTexture = new ModelTexture(loader.loadTexture("mario.png"));
+		RawModel rawModel = OBJLoader.loadObjModel("stall.obj", loader);
+		ModelTexture modelTexture = new ModelTexture(loader.loadTexture("stallTexture.png"));
 		TexturedModel texturedModel = new TexturedModel(rawModel, modelTexture);
 
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -1), 0, 0, 0, 1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
 
 		Camera camera = new Camera();
 
 		while (!Display.isCloseRequested()) {
-			entity.increasePosition(0, 0, -0.002f);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			// game logic
